0ec6ee2edffabb69795d5bc308335a4beeb77921
Talah5 <hourybastien@gmail.com>
2024-10-24 12:48:04 +0200

    Mise à jour de table.h

--- include/table.h
+++ include/table.h
@@ -4,30 +4,21 @@
 // Structure représentant une ligne dans la table
 typedef struct {
     int id;
-    char name[100];  // Exemple : un nom avec un maximum de 100 caractères
-} Row;
+    char name[100];  // Un nom avec un maximum de 100 caractères
+} Record;
 
-// Structure pour un nœud de l'arbre binaire
+// Structure pour un nœud dans l'arbre binaire
 typedef struct Node {
-    Row data;
+    Record data;
     struct Node* left;
     struct Node* right;
 } Node;
 
-// Structure de la table avec un arbre binaire
-typedef struct {
-    Node* root;  // Racine de l'arbre binaire
-} Table;
-
 // Prototypes des fonctions
-Table* create_table();
-void free_table(Table* table);
-int insert_row(Table* table, int id, const char* name);
-Row* select_row(const Table* table, int id);
-int delete_row(Table* table, int id);
-int update_row(Table* table, int id, const char* new_name);  // Nouveau : mise à jour
-void print_table(const Table* table);
-int save_table_to_file(const Table* table, const char* filename);
-int load_table_from_file(Table* table, const char* filename);
+Node* create_node(int id, const char* name);
+Node* insert_node(Node* root, int id, const char* name);
+Node* delete_node(Node* root, int id);
+void print_inorder(Node* root);
+void free_tree(Node* root);
 
 #endif
